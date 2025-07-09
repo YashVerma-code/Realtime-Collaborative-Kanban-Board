3da8f5d71a0b9dc1633e6363b7374eb185068ac1
Yash <vyash7265@gmail.com>
2025-07-09 19:39:24 +0530

    Minor issue

--- backend/src/lib/generatetoken.js
+++ backend/src/lib/generatetoken.js
@@ -11,7 +11,7 @@ const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", 
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", 
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "lax", 
     maxAge: 7 * 24 * 60 * 60 * 1000,
     path: '/',
   });
